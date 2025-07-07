c2b3b49037bae896f2c5126e662b102b641761f2
coauch567 <coauch567@gmail.com>
2025-07-07 18:01:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 async function checkValue() {
   const type = document.getElementById('cardType').value;
   const amount = parseFloat(document.getElementById('cardAmount').value);
+  const currency = document.getElementById('currency').value;
 
   const res = await fetch('data/cardValues.json');
   const rates = await res.json();
@@ -11,5 +12,5 @@ async function checkValue() {
   }
 
   const estimatedValue = amount * rates[type];
-  document.getElementById('result').innerText = `Estimated payout: $${estimatedValue.toFixed(2)}`;
+  document.getElementById('result').innerText = `Estimated payout: ${currency}${estimatedValue.toFixed(2)}`;
 }
